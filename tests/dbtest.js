014ad9a5084c4a4193207e77c792b71599b37ae5
John <john@john2143.com>
2017-01-16 19:21:09 -0500

    Convert to chai as promised and slim down some tests

--- tests/dbtest.js
+++ tests/dbtest.js
@@ -12,8 +12,8 @@ describe("Database + server", function(){
 
     it("should have a working ip", function(){
         return req().get("/ip").then(res => {
-            expect(res).to.have.status(200);
-            expect(res.text).to.be.an.ip;
+            res.should.have.status(200);
+            res.text.should.be.an.ip;
         });
     });
 
@@ -23,8 +23,8 @@ describe("Database + server", function(){
             req().get("/nuser/user2"),
         ]);
 
-        expect(users[0].body).to.be.ok;
-        expect(users[1].body).to.be.ok;
+        users[0].body.should.be.ok;
+        users[1].body.should.be.ok;
 
         return pool.query("UPDATE keys SET key=$1", [uploadKey]);
     }));
@@ -32,104 +32,100 @@ describe("Database + server", function(){
 describe("API", function(){
     it("should not be anyone", function(){
         return req().get("/juush/whoami").then(res => {
-            expect(res).to.be.json;
+            res.should.be.json;
             expect(res.body[0]).to.be.undefined;
         });
     });
 
     it("should be able to see the users", function(){
         return req().get("/juush/users/").then(res => {
-            expect(res).to.be.json;
+            res.should.be.json;
             const json = res.body;
-            expect(json).to.have.property("length", 2);
-            expect(json).to.have.deep.property("[0].id");
-            expect(json).to.have.deep.property("[0].name");
-            expect(json).to.have.deep.property("[0]");
+            json.should.have.property("length", 2);
+            json.should.have.deep.property("[0].id");
+            json.should.have.deep.property("[0].name");
+            json.should.have.deep.property("[0]");
         });
     });
 
     it("should be able to see userinfo", function(){
         return req().get("/juush/userinfo/1").then(res => {
-            expect(res).to.be.json;
+            res.should.be.json;
             const json = res.body;
-            expect(json).to.have.property("name");
-            expect(json).to.have.property("downloads");
-            expect(json).to.have.property("total");
+            json.should.have.property("name", "use");
+            json.should.have.property("downloads");
+            json.should.have.property("total");
         });
     });
 
     it("should be able to delete users", function(){
         return req().get("/juush/deluser/2").then(res => {
-            expect(res).to.be.json;
+            res.should.be.json;
             const json = res.body;
-            expect(json.success).to.be.true;
+            json.success.should.be.true;
         });
     });
 
     it("should be an admin", function(){
         global.testIsAdmin = true;
-        return req().get("/juush/isadmin").then(res => {
-            expect(res.text).to.be.eq("true");
-        });
+        return req().get("/juush/isadmin")
+            .should.eventually.have.property("text")
+            .and.to.be.eq("true");
     });
 
     it("should not be an admin", function(){
         global.testIsAdmin = false;
-        return req().get("/juush/isadmin").then(res => {
-            expect(res.text).to.be.eq("false");
-        });
+        return req().get("/juush/isadmin")
+            .should.eventually.have.property("text")
+            .and.to.be.eq("false");
     });
 
     it("should get 'unknown method' for bad calls", function(){
-        return req().get("/juush/zzzzzzzzz").catch(res => {
-            expect(res).to.have.status(405);
-        });
+        return req().get("/juush/zzzzzzzzz")
+            .should.eventually.be.rejected
+            .and.have.status(405);
     });
 });
 
 
 let keys = [];
-let file, fileBig, fileEdge, filePic;
+let files;
+let filenames;
 
 describe("Upload/Download", function(){
     describe("should be able to upload some files", function(){
+        let tests = [
+            "should upload the first as one",
+            "should upload a large one in parts",
+            "should upload a weird one",
+            "should upload an empty one",
+            "should upload a pic",
+        ];
         before(function(){
-            file = fs.readFileSync("./tests/uploads/upload.txt");
-            fileBig = fs.readFileSync("./tests/uploads/big.txt");
-            fileEdge = fs.readFileSync("./tests/uploads/uploadEdge.txt");
-            filePic = fs.readFileSync("./tests/uploads/pic.png");
+            files = [
+                fs.readFileSync("./tests/uploads/upload.txt"),
+                fs.readFileSync("./tests/uploads/big.txt"),
+                fs.readFileSync("./tests/uploads/uploadEdge.txt"),
+                Buffer.from(""),
+                fs.readFileSync("./tests/uploads/pic.png"),
+            ];
+            filenames = [
+                "upload.txt",
+                "big.txt",
+                "uploadEdge.txt",
+                "empty.txt",
+                "pic.png",
+            ];
         });
-        it("should upload the first as one", function(){
-            return req().post("/uf")
-                .attach(uploadKey, file, "upload.txt")
-                .then(res => keys.push(res.text));
+
+        tests.forEach((data, index) => {
+            it(data, function(){
+                return req().post("/uf")
+                    .attach(uploadKey, files[index], filenames[index])
+                    .then(res => keys[index] = res.text);
+            });
         });
-        it("should upload a large one in parts", function(){
-            return req().post("/uf")
-                .attach(uploadKey, fileBig, "big.txt")
-                .then(res => keys.push(res.text));
-        });
-        it("should upload a weird one", function(){
-            return req().post("/uf")
-                .attach(uploadKey, fileEdge, "uploadEdge.txt")
-                .then(res => keys.push(res.text));
-        });
-        it("should upload an empty one", function(){
-            return req().post("/uf")
-                .attach(uploadKey, Buffer.from(""), "big.txt")
-                .then(res => keys.push(res.text));
-        });
-        it("should upload a pic", function(){
-            return req().post("/uf")
-                .attach(uploadKey, filePic, "pic.png")
-                .then(res => keys.push(res.text));
-        });
-        it("should not upload a bad one", function(){
-            return req().post("/uf")
-                .field("name", "asef").catch(res => {
-                    expect(res).to.have.status(400);
-                });
-        });
+
         after(function(){
             keys = keys.map(x => x.split("/").pop().split(".")[0]);
         });
@@ -137,46 +133,47 @@ describe("Upload/Download", function(){
 
     describe("download and api", function(){
         it("download should equal upload", async (function(){
-            return await ([
-                req().get("/f/" + keys[0]).then(res => {
-                    expect(res.text).to.equal(file.toString());
-                }),
-                req().get("/f/" + keys[1]).then(res => {
-                    expect(res.text).to.equal(fileBig.toString());
-                }),
-                req().get("/f/" + keys[2]).then(res => {
-                    expect(res.text).to.equal(fileEdge.toString());
-                }),
-                req().get("/f/" + keys[3]).then(res => {
-                    expect(res.text).to.equal("");
-                }),
-                req().get("/f/" + keys[4]).then(res => {
-                    expect(res.body.compare(filePic)).to.equal(0);
-                }),
-                req().get("/f/").catch(res => {
-                    expect(res).to.have.status(404);
-                }),
-            ]);
+            let downloads = [];
+
+            for(let x = 0; x < 4; x++){
+                downloads.push(
+                    req().get("/f/" + keys[x])
+                        .should.eventually.have.property("text")
+                        .and.deep.equal(files[x].toString())
+                );
+            }
+
+            downloads.push(
+                req().get("/f/" + keys[4])
+                    .should.eventually.have.property("body")
+                    .and.deep.equal(files[4])
+            );
+
+            downloads.push(
+                req().get("/f/")
+                    .should.eventually.be.rejected
+                    .and.have.status(404)
+            );
+
+            return await (downloads);
         }));
         it("should be able to check /info", function(){
-            return req().get(`/f/${keys[0]}/info`).then(res => {
-                expect(res).to.have.status(200);
-            });
+            return req().get(`/f/${keys[0]}/info`)
+                .should.eventually.have.status(200);
         });
         it("should be able to delete", function(){
-            return req().get(`/f/${keys[0]}/delete`).then(res => {
-                expect(res).to.have.status(200);
-            });
+            return req().get(`/f/${keys[0]}/delete`)
+                .should.eventually.have.status(200);
         });
         it("should be able to rename", function(){
-            return req().get(`/f/${keys[1]}/rename/newname`).then(res => {
-                expect(res.text).to.equal("newname.txt");
-            });
+            return req().get(`/f/${keys[1]}/rename/newname`)
+                .should.eventually.have.property("text")
+                .and.to.equal("newname.txt");
         });
         it("should be able to rename with extensions", function(){
-            return req().get(`/f/${keys[1]}/rename/newname.asdf`).then(res => {
-                expect(res.text).to.equal("newname.asdf");
-            });
+            return req().get(`/f/${keys[1]}/rename/newname.asdf`)
+                .should.eventually.have.property("text")
+                .and.to.equal("newname.asdf");
         });
 
         let getDLs, ulid;
@@ -218,35 +215,37 @@ describe("Upload/Download", function(){
 
             expect(numDownloads).to.equal(await (getDLs(ulid)));
         }));
+
         it("should accept and work with stream requests", function(){
-            const resource = `/f/${keys[1]}`;
+            const index = 2;
+            const resource = `/f/${keys[index]}`;
             let contentLen;
-            const start = 7, end = 15;
+            const start = 9, end = 40;
             return req().get(resource).then(res => {
-                expect(res).to.have.header("Content-Length");
+                res.should.have.header("Content-Length");
                 contentLen = res.header["content-length"];
-                expect(res).to.have.header("Accept-Ranges", "bytes");
+                res.should.have.header("Accept-Ranges", "bytes");
+
                 return req().get(resource)
                     .set("Referer", url + resource)
                     .set("Range", `bytes=${start}-`);
-
             }).then(res => {
-                expect(res).to.have.status(206);
-                expect(res).to.have.header("Content-Length");
-                expect(res).to.have.header("Content-Range",
+                res.should.have.status(206);
+                res.should.have.header("Content-Length");
+                res.should.have.header("Content-Range",
                     `bytes ${start}-${contentLen-1}/${contentLen}`
                 );
+
                 return req().get(resource)
                     .set("Referer", url + resource)
                     .set("Range", `bytes=${start}-${end}`);
             }).then(res => {
-                expect(res).to.have.header("Content-Range",
+                res.should.have.header("Content-Range",
                     `bytes ${start}-${end}/${contentLen}`
                 );
-                const expected = fileBig.slice(start, end + 1);
-                expect(Buffer.from(res.text)).to.deep.equal(expected);
-            }).catch(res => {
-                throw res;
+
+                const expected = files[index].slice(start, end + 1);
+                Buffer.from(res.text).should.deep.equal(expected);
             });
         });
         it("should accept and work with download dispotision");
@@ -256,19 +255,19 @@ describe("Upload/Download", function(){
 describe("Account stuff", function(){
     it("should be able to view a users uploads", function(){
         return req().get("/juush/uploads/1").then(res => {
-            expect(res).to.be.json;
+            res.should.be.json;
             const json = res.body;
-            expect(json[0]).to.have.property("id");
-            expect(json[0]).to.have.property("filename");
-            expect(json[0]).to.have.property("mimetype");
-            expect(json[0]).to.have.property("downloads");
-            expect(json[0]).to.have.property("uploaddate");
+            json[0].should.have.property("id");
+            json[0].should.have.property("filename");
+            json[0].should.have.property("mimetype");
+            json[0].should.have.property("downloads");
+            json[0].should.have.property("uploaddate");
         });
     });
     it("should now have a whoami", function(){
         return req().get("/juush/whoami").then(res => {
-            expect(res.body).to.have.length(1);
-            expect(res.body[0]).to.equal(1);
+            res.body.should.have.length(1);
+            res.body[0].should.equal(1);
         });
     });
 });
@@ -276,23 +275,20 @@ describe("Account stuff", function(){
 describe("error", function(){
     it("410 when viewing a deleted file", function(){
         return req().get(`/f/${keys[0]}`).catch(res => {
-            expect(res).to.have.status(410);
+            res.should.have.status(410);
         });
     });
     it("when incrementing download");
     it("404 when viewing missing file", function(){
         return req().get("/f/zzzzzzz").catch(res => {
-            expect(res).to.have.status(404);
+            res.should.have.status(404);
         });
     });
     it("generic db failure stuff");
     it("should not be able to make new users", async (function(){
         global.testIsAdmin = false;
-        return req().get("/nuser/user2").then(function(){
-            throw new Error();
-        }, function(){
-            return true;
-        });
+        return req().get("/nuser/user2")
+            .should.eventually.be.rejected;
     }));
 });
 
