7f24ac5f4392a579d92d2e5498299d2511d8131d
John Schmidt <john@john2143.com>
2020-09-13 18:19:53 -0400

    delete node files

--- src/juush/index.js
+++
@@ -1,9 +0,0 @@
-serverLog("Creating juush");
-
-import * as U from "./util.js";
-
-import API from "./API.js";
-import download from "./download.js";
-import upload from "./upload.js";
-import newUser from "./newuser.js";
-export {API, download, upload, newUser, U};
