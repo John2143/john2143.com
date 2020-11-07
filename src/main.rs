93c05c79207cdb73de32d13d0a4e4654bd225e6f
John Schmidt <john@john2143.com>
2020-11-07 13:31:44 -0500

    temp commit

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,50 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_http::{http, Request, Response};
+use actix_multipart::Multipart;
+use actix_web::{
+    get, middleware, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use futures::{StreamExt, TryStreamExt};
 
+#[post("/uf")]
+async fn upload(req: HttpRequest, mut payload: Multipart) -> impl Responder {
+    if let Ok(Some(mut field)) = payload.try_next().await {
+        println!("{:?}", field);
+        println!("{}", field.content_disposition().unwrap());
+    }
+
+    "https://localhost:8000/test"
+}
+
+#[get("/test/thumb")]
+async fn testthumb(req: HttpRequest) -> impl Responder {
+    format!("Hello World")
+}
+
+#[get("/test")]
 async fn test(req: HttpRequest) -> impl Responder {
     format!("Hello World")
 }
 
-const redirects: &[(&str, &str)] = &[
-    ("/", "https://github.com/John2143"),
-];
-
-use actix_web::body::Body;
-fn create_server(app: &mut App<AppEntry, Body>) {
-    let mut app = App::new();
-
-    for (src, dest) in redirects.into_iter() {
-    }
-}
+const REDIRECTS: &[(&'static str, &'static str)] = &[("/", "https://github.com/John2143")];
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         let mut app = App::new();
-        create_server(&mut app);
+        for (src, dest) in REDIRECTS.iter() {
+            println!("Route: {} -> {}", src, dest);
+            let svc = web::resource(*src).route(web::get().to(move || {
+                println!("hit");
+                HttpResponse::MovedPermanently()
+                    .header(http::header::LOCATION, *dest)
+                    .finish()
+            }));
+
+            app = app.service(svc);
+        }
+        app = app.service(test);
+        app = app.service(upload);
+        app = app.service(testthumb);
         app
     })
     .bind("0.0.0.0:8000")?
